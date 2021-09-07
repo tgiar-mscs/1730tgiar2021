6289f6e984f071c82c9a5d6c07182a394cad8709
Tylor <uk8527gy@Go.MinnState.edu>
2021-09-07 13:49:39 -0500

    Completed ex1d

---
+++ tgiar1d1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tgiar1d1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAmountAustralia.Text = "0.00";
+            txtRateAustralia.Text = "0.7179976";
+            txtAmountBhutan.Text = "0.00";
+            txtRateBhutan.Text = "0.0139831";
+            txtAmountCostaRica.Text = "0.00";
+            txtRateCostaRica.Text = "0.00176122";
+            txtAmountEuro.Text = "0.00";
+            txtRateEuro.Text = "1.15528";
+
+
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void australiaTextChanged(object sender, EventArgs e)
+        {
+            txtUSDAustralia.Text = (
+                Convert.ToDecimal(txtAmountAustralia.Text) * Convert.ToDecimal(txtRateAustralia.Text)
+                ).ToString("0.00");
+
+        }
+
+        private void BhutanTextChanged(object sender, EventArgs e)
+        {
+            txtUSDBhutan.Text = (
+                Convert.ToDecimal(txtAmountBhutan.Text) * Convert.ToDecimal(txtRateBhutan.Text)
+                ).ToString("0.00");
+        }
+
+        private void CostaRicaTextChanged(object sender, EventArgs e)
+        {
+            txtUSDCostaRica.Text = (
+                Convert.ToDecimal(txtAmountCostaRica.Text) * Convert.ToDecimal(txtRateCostaRica.Text)
+                ).ToString("0.00");
+        }
+
+        private void EuroTextChanged(object sender, EventArgs e)
+        {
+            txtUSDEuro.Text = (
+                Convert.ToDecimal(txtAmountEuro.Text) * Convert.ToDecimal(txtRateEuro.Text) 
+                ).ToString("0.00");
+        }
+
+        private void usdTextChanged(object sender, EventArgs e)
+        {
+            txtTotalUSD.Text = (
+               Convert.ToDecimal(txtUSDAustralia.Text) + 
+               Convert.ToDecimal(txtUSDBhutan.Text) + 
+               Convert.ToDecimal(txtUSDCostaRica.Text) + 
+               Convert.ToDecimal(txtUSDEuro.Text)
+                ).ToString("0.00");
+        }
+    }
+}
